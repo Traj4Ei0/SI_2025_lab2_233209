6ca89872e460daa2da05d4e62e6630dd6a938196
Traj4Ei0 <kostadinovtrajce3@gmail.com>
2025-05-15 23:37:53 +0200

    Update SILab2Test.java

--- src/main/test/SILab2Test.java
+++ src/main/test/SILab2Test.java
@@ -2,35 +2,33 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 public class SILab2Test {
-
-    // Every Statement coverage
+    
     @Test
     public void testEveryStatement() {
-        // null list
+     
         RuntimeException e1 = assertThrows(RuntimeException.class, () -> SILab2.checkCart(null, "1234567890123456"));
         assertEquals("allItems list can't be null!", e1.getMessage());
 
-        // invalid item name
+      
         List<Item> items2 = List.of(new Item(null, 100, 0, 1));
         RuntimeException e2 = assertThrows(RuntimeException.class, () -> SILab2.checkCart(items2, "1234567890123456"));
         assertEquals("Invalid item!", e2.getMessage());
 
-        // invalid card number (length != 16)
+    
         List<Item> items3 = List.of(new Item("item", 100, 0, 1));
         RuntimeException e3 = assertThrows(RuntimeException.class, () -> SILab2.checkCart(items3, "123"));
         assertEquals("Invalid card number!", e3.getMessage());
 
-        // valid data, no condition triggered
+       
         List<Item> items4 = List.of(new Item("item", 100, 0, 1));
         assertEquals(100, SILab2.checkCart(items4, "1234567890123456"));
 
-        // valid data, card contains invalid character
         List<Item> items5 = List.of(new Item("item", 100, 0, 1));
         RuntimeException e5 = assertThrows(RuntimeException.class, () -> SILab2.checkCart(items5, "12345678901234a6"));
         assertEquals("Invalid character in card number!", e5.getMessage());
     }
 
-    // Multiple Condition coverage for: price > 300 || discount > 0 || quantity > 10
+    
     @Test
     public void testMultipleConditionPriceDiscountQuantity() {
         String card = "1234567890123456";
